1892ec26a0a152fe7e0838f7e3f874ba2db89368
leofaysal <alizafaysal@gmail.com>
2021-07-09 15:02:10 +0500

    09-07-2021

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,68 @@
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+              Admin
+                <small>Subheading</small>
+            </h1>
+
+            <?php
+            echo INCLUDES_PATH;
+              // $user = new User();
+              // $user->username = "new_user";
+              // $user->password = "1234";
+              // $user->first_name = "student";
+              // $user->last_name = "user";
+              //
+              // $user->create();
+
+               // $user=User::find_user_by_id(10);
+               // $user->first_name ="Faseee";
+               // $user->update();
+               // $user=User::find_user_by_id(8);
+               // $user->delete();
+              // $user=User::find_user_by_id(11);
+              // $user->last_name ="ali";
+              //  $user->password = "1234";
+              // $user->save();
+            //   $user=User::find_user_by_id(4);
+               // $user = new User();
+               // $user->username ="ali";
+               // $user->save();
+
+                 // $users=User::find_all();
+                 //
+                 // foreach ($users as $user){
+                 //   echo $user->username;
+                 // }
+                 // $photo = new Photo();
+                 // $photo->title = "new_car";
+                 // $photo->description = "This is a new latest model sports car";
+                 // $photo->filename = "student.jpg";
+                 // $photo->type = "new image";
+                 // $photo->size = 20;
+                 // $photo->create();
+
+                 // $photos=Photo::find_all();
+                 //
+                 // foreach ($photos as $photo){
+                 //   echo $photo->title;
+                 // }
+
+            ?>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
+        </div>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
